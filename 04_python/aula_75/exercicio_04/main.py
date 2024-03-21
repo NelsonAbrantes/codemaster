fe2baa2b54241c9ed7c1fe3acf94e1569515aa83
NelsonAbrantes <n.abrantes@hotmail.com>
2024-03-22 00:31:49 +0100

    aula 75

---
+++ 04_python/aula_75/exercicio_04/main.py
@@ -0,0 +1,28 @@
+
+x = 8
+y = 3
+
+soma = x + y
+sub = x - y
+mult = x * y
+div = x / y
+
+div_int = x // y
+potencia = x ** y
+resto = x % y
+
+print("\n")
+
+print("=== Operações Aritméticas ===")
+print("\nX =(8) \nY = (3) \n")
+
+print("Soma = (" + str(soma) + ")")
+print("Subtração = (" + str(sub) + ")")
+print("Multiplicação = (" + str(mult) + ")")
+print("Divisão = (" + str(div) + ")")
+print("Divisão inteira = (" + str(div_int) + ")\n")
+
+print("Potência = (" + str(potencia) + ")")
+print("Resto da divisão = (" + str(resto) + ")")
+
+
---
+++ 04_python/aula_75/exercicio_1/main.py
@@ -0,0 +1,5 @@
+print("\n")
+
+print("===Dados=== \nNome: (Nelson Abrantes) \nMorada: (Amriswil) \nIdade: (34)")
+
+print("\n")
