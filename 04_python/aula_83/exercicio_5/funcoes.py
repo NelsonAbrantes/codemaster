1e58ce158994492e67e78fb030aec3a5a34431be
NelsonAbrantes <n.abrantes@hotmail.com>
2024-04-04 11:40:43 +0200

    aula_83

--- 04_python/aula_83/exercicio_5/funcoes.py
+++ 04_python/aula_83/exercicio_5/funcoes.py
@@ -1,18 +1,39 @@
 import os
 import time
 
-def desconto(preco):
-    novo_preco = preco * 0.9
-    print(f"O novo preco é de {novo_preco} Euros.")
+def esperar():
+    tempo = 0.8
+    limpar()
+    print("A analisar.")
+    aguardar(tempo)
+    limpar()
+    print("A analisar..")
+    aguardar(tempo)
+    limpar()
+    print("A analisar...")
+    aguardar(tempo)
+    limpar()
 
-def desconto_2(preco):
-    novo_preco = preco * 0.9
-    return novo_preco
+def candidatura(idade, conhecimentos):
 
+    if(idade >= 18 and conhecimentos.lower() == "sim"):
+        return "Aprovado para estágio".upper()
+    if(idade <= 18 and conhecimentos.lower() == "sim"):
+        return "Aprovado para escola de Programadores".upper()
+    if(idade >= 18 and conhecimentos.lower() == "nao"):
+        return "Não Aprovado para estágio".upper()
+    if(idade <= 18 and conhecimentos.lower() == "nao"):
+        return "Não aprovado para escola de programadores".upper()
+    else:
+        return "Erro nos dados informados, tente novamente".upper()
+    
+    
 
+#-- funcoes especiais --
 def limpar():
     if(os.name == "nt"): os.system("cls")
     else: os.system("clear")
 
 def aguardar(tempo):
     time.sleep(tempo)
+
--- 04_python/aula_83/exercicio_5/main.py
+++ 04_python/aula_83/exercicio_5/main.py
@@ -1,1 +1,11 @@
 from funcoes import *
+
+nome = input(" Digite o seu nome: ")
+idade = int(input(" Digite a sua idade: "))
+conhecimentos = input(" Tem conhecimentos em programação? ")
+
+limpar()
+esperar()
+
+print("=== Ficha de candidatura ===\n")
+print(f"Nome: ( {nome} )\nidade: ( {idade} )\nStatus da candidatura: ( {candidatura(idade, conhecimentos)} )")
